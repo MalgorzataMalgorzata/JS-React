7d6f391d29d80d71491d19b53ebac0054cf9a44b
MalgorzataPrucnal <58142596+MalgorzataPrucnal@users.noreply.github.com>
2019-12-08 14:01:37 +0100

    Update wisielec.js

--- wisielec.js
+++ wisielec.js
@@ -1,4 +1,4 @@
-function check(){ 
+function check() { 
     const slowa = ['kakadu', 'koala', 'kangur', 'koczkodan'];
     const index = Math.floor(Math.random() * slowa.length);
     const slowo = Array.from(slowa[index]);
@@ -7,26 +7,27 @@ function check(){
     let hidden = slowo.map(hide);
     alert('Szukane słowo: ' + hidden.join(' '));
 
-    while ((JSON.stringify(hidden) !== JSON.stringify(slowo)) && liczbaBledow > 0) 
-    {
+    while ((JSON.stringify(hidden) !== JSON.stringify(slowo)) && liczbaBledow > 0) {
         let wpisanaLitera = prompt('Wpisz literę: ');
         let pozycja = slowo.indexOf(wpisanaLitera);
-        if (pozycja !== -1)
-            {    
-                for (i = 0; i <= slowo.length; i+=1){
-                (slowo[i] === wpisanaLitera) && (hidden[i] = wpisanaLitera); 
-                }
-                console.log(hidden.join(''));
+            if (wpisanaLitera.length > 1) {
+                alert ("Za dużo znaków. Wpisz tylko jedną literę.");
             }
-        else 
-            {
-            liczbaBledow -= 1; console.log('W tym wyrazie nie ma tej litery.')
-            };
+            else {
+                if (pozycja !== -1) {    
+                    for (i = 0; i <= slowo.length; i+=1) {
+                    (slowo[i] === wpisanaLitera) && (hidden[i] = wpisanaLitera); 
+                    }
+                    alert(hidden.join(' '));
+                    }
+                else {
+                    liczbaBledow -= 1; alert('W tym wyrazie nie ma tej litery.')
+                }
+            }
     }
-    (liczbaBledow === 0) && (console.log('Koniec gry! Spróbuj jeszcze raz.'));
-    (JSON.stringify(hidden)=== JSON.stringify(slowo)) && console.log('Pięknie! Szukane słowo to: ' + hidden.join('') + '.');
+    (liczbaBledow === 0) && (alert('Koniec gry! Spróbuj jeszcze raz.'));
+    (JSON.stringify(hidden)=== JSON.stringify(slowo)) && alert('Pięknie! Szukane słowo to ' + hidden.join('') + '.');
 }
 
     check();
-
     
